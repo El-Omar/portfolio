e70c0642d7147fcd2bcd080c18ad77ed19685959
El Omar Sami <elomar.sami@skarabee.com>
2021-01-17 15:36:03 +0100

    work in progress message

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -62,115 +62,123 @@ const IndexPage = () => {
     }
   `);
 
-  const spaceInvadersDivs = Array(225).fill('block');
-  let $spaceInvaders = useRef(null);
+  // const spaceInvadersDivs = Array(225).fill('block');
+  // let $spaceInvaders = useRef(null);
 
-  useEffect(() => {
-    spaceInvaders($spaceInvaders);
-  }, [$spaceInvaders]);
+  // useEffect(() => {
+  //   spaceInvaders($spaceInvaders);
+  // }, [$spaceInvaders]);
 
-  const projectRefs = useRef([]);
-  const [projects, setProjects] = useState([]);
+  // const projectRefs = useRef([]);
+  // const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    let projects = data.projects.edges;
-    projectRefs.current = new Array(projects.length);
-    setProjects(projects);
-    console.log("projects before focussing ")
-  }, [data.projects.edges]);
+  // useEffect(() => {
+  //   let projects = data.projects.edges;
+  //   projectRefs.current = new Array(projects.length);
+  //   setProjects(projects);
+  //   console.log("projects before focussing ")
+  // }, [data.projects.edges]);
 
-  useEffect(() => {
-    if (projects.length > 0) {
-      projectRefs.current[projects.length - 1].focus();
-    }
-    console.log("projects in focus: ", projects);
-  }, [projects]);
+  // useEffect(() => {
+  //   if (projects.length > 0) {
+  //     projectRefs.current[projects.length - 1].focus();
+  //   }
+  //   console.log("projects in focus: ", projects);
+  // }, [projects]);
 
   return (
-    <Layout>
-      <SEO title="Home" />
-      <section className="wrapper wrapper--welcome">
-        <header>
-          <strong className="subtitle">What's up!</strong>
-          <h1 className="wrapper__title">
-            Hi! My name is El Omar, <br />
-            and I love <span className="italic">designing <br /> 
-            experiences</span><span className="color-primary">.</span>
-          </h1>
-        </header>
-        <Img 
-          className="elomar__img" 
-          alt="El Omar" 
-          fluid={data.placeholderImage.childImageSharp.fluid} 
-          imgStyle={{ objectPosition: '50% 100%', objectFit: 'contain' }} />
-      </section>
+    <header style={{ marginTop: 0, height: `100%`, display: `flex`, justifyContent: `center`, padding: `5rem`, flexFlow: `column`, textAlign: `center`, }}>
+      <strong className="subtitle">What's up!</strong>
+      <h1 className="wrapper__title" style={{ lineHeight: `2.8rem` }}>
+        This is a web designer, who is working on his personal website at the moment.<br />
+        <span className="italic">Be patient</span><span className="color-primary">.</span><br />
+        <a href="mailto:elomar.sami@gmail.com">elomar.sami@gmail.com</a>
+      </h1>
+    </header>
+    // <Layout>
+    //   <SEO title="Home" />
+    //   <section className="wrapper wrapper--welcome">
+    //     <header>
+    //       <strong className="subtitle">What's up!</strong>
+    //       <h1 className="wrapper__title">
+    //         Hi! My name is El Omar, <br />
+    //         and I love <span className="italic">designing <br /> 
+    //         experiences</span><span className="color-primary">.</span>
+    //       </h1>
+    //     </header>
+    //     <Img 
+    //       className="elomar__img" 
+    //       alt="El Omar" 
+    //       fluid={data.placeholderImage.childImageSharp.fluid} 
+    //       imgStyle={{ objectPosition: '50% 100%', objectFit: 'contain' }} />
+    //   </section>
 
-      <section className="wrapper wrapper--expertise">
-        <header className="wrapper__header wrapper__header--center">
-          <h1 className="wrapper__title">
-            What can <br />
-            <span className="italic">I do</span>?
-          </h1>
-        </header>
+    //   <section className="wrapper wrapper--expertise">
+    //     <header className="wrapper__header wrapper__header--center">
+    //       <h1 className="wrapper__title">
+    //         What can <br />
+    //         <span className="italic">I do</span>?
+    //       </h1>
+    //     </header>
 
-        { skillsets.map(skillset => {
-          return <Skillset key={ skillset.id } { ...skillset } />
-        }) }
-      </section>
+    //     { skillsets.map(skillset => {
+    //       return <Skillset key={ skillset.id } { ...skillset } />
+    //     }) }
+    //   </section>
 
-      <section className="wrapper wrapper--projects">
-        <header className="wrapper__header">
-          <strong className="subtitle">My work!</strong>
-          <h1 className="wrapper__title">Projects</h1>
-        </header>
+    //   <section className="wrapper wrapper--projects">
+    //     <header className="wrapper__header">
+    //       <strong className="subtitle">My work!</strong>
+    //       <h1 className="wrapper__title">Projects</h1>
+    //     </header>
 
-        <div className="projects__wrap">
-          { data.projects.edges.map(({ node: project }, i) => {
-            return (
-              <article ref={el => projectRefs.current[i] = el}
-                className="project" key={project.slug}>
-                <Link to={`/projects/${project.slug}/`}>
-                  <Img 
-                    fluid={project.images[0].fluid }
-                    key={project.images[0].title}
-                    alt={project.images[0].title}
-                    style={{ height: `100%`, }}
-                  />
-                </Link>
-              </article>
-            )
-          }) }
-        </div>
-      </section>
+    //     <div className="projects__wrap">
+    //       { data.projects.edges.map(({ node: project }, i) => {
+    //         return (
+    //           <article ref={el => projectRefs.current[i] = el}
+    //             className="project" key={project.slug}>
+    //             <Link to={`/projects/${project.slug}/`}>
+    //               <Img 
+    //                 fluid={project.images[0].fluid }
+    //                 key={project.images[0].title}
+    //                 alt={project.images[0].title}
+    //                 style={{ height: `100%`, }}
+    //               />
+    //             </Link>
+    //           </article>
+    //         )
+    //       }) }
+    //     </div>
+    //   </section>
 
-      <section className="wrapper wrapper--contact">
-        <header className="wrapper__header wrapper__header--center">
-          <strong className="subtitle">Get in touch</strong>
-          <h1 className="wrapper__title">Wanna get to <br/> 
-          know me <span className="italic">& play <br/>Space Invaders</span><span className="color-primary">?</span></h1>
-        </header>
+    //   <section className="wrapper wrapper--contact">
+    //     <header className="wrapper__header wrapper__header--center">
+    //       <strong className="subtitle">Get in touch</strong>
+    //       <h1 className="wrapper__title">Wanna get to <br/> 
+    //       know me <span className="italic">& play <br/>Space Invaders</span><span className="color-primary">?</span></h1>
+    //     </header>
 
-        <div className="container">
-          <a className="btn-contact" href="mailto:hi@elomar.be">Get in touch</a>
-          <div className="spaceinvaders" ref={el => $spaceInvaders = el}>
-            <div className="game">
-              { 
-                spaceInvadersDivs.map((_, i) => (
-                  <div className={_} key={i}></div>
-                ))
-              }
-            </div>
-            <div className="game-info">
-              <button className="btn-play">
-                <span className="play">Play</span>
-                <span className="result"></span>
-              </button>
-              <span className="high-score">High score: <span className="value">0</span></span>
-            </div>
-          </div>
-        </div>
-      </section>
-    </Layout>
+    //     <div className="container">
+    //       <a className="btn-contact" href="mailto:hi@elomar.be">Get in touch</a>
+    //       <div className="spaceinvaders" ref={el => $spaceInvaders = el}>
+    //         <div className="game">
+    //           { 
+    //             spaceInvadersDivs.map((_, i) => (
+    //               <div className={_} key={i}></div>
+    //             ))
+    //           }
+    //         </div>
+    //         <div className="game-info">
+    //           <button className="btn-play">
+    //             <span className="play">Play</span>
+    //             <span className="result"></span>
+    //           </button>
+    //           <span className="high-score">High score: <span className="value">0</span></span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </section>
+    // </Layout>
   )
 }
 
